3cc88e249519357fee2464da68a03a62a41a2a71
crocomoth <justletmeplaymf@gmail.com>
2018-06-24 16:42:58 +0300

    HiveServiceTests and HiveSectionService tests added.

---
+++ KatlaSport.Services.Tests/HiveManagement/HiveSectionServiceTests.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
+using AutoFixture.Xunit2;
+using KatlaSport.DataAccess.ProductStoreHive;
+using KatlaSport.Services.HiveManagement;
+using Moq;
+using Xunit;
+
+namespace KatlaSport.Services.Tests.HiveManagement
+{
+    public class HiveSectionServiceTests
+    {
+        [Fact]
+        public async void GetHiveSectionAsyncSuccessfull()
+        {
+            var productContext = new Mock<IProductStoreHiveContext>();
+            productContext.Setup(p => p.Hives).ReturnsEntitySet(new List<StoreHive>());
+
+            var userContext = new Mock<IUserContext>();
+            userContext.Setup(u => u.UserId).Returns(1);
+
+            var service = new HiveSectionService(productContext.Object, userContext.Object);
+
+            var sections = await service.GetHiveSectionsAsync();
+            Assert.NotNull(sections);
+        }
+
+        public async void GetHiveSectionsNotEmpty()
+        {
+            var productContext = new Mock<IProductStoreHiveContext>();
+            productContext.Setup(p => p.Hives).ReturnsEntitySet(new List<StoreHive>());
+
+            var userContext = new Mock<IUserContext>();
+            userContext.Setup(u => u.UserId).Returns(1);
+
+            var service = new HiveSectionService(productContext.Object, userContext.Object);
+
+            var sections = await service.GetHiveSectionsAsync();
+            Assert.Empty(sections);
+        }
+
+        [Fact]
+        public async void CreateHiveSectionSuccesFull()
+        {
+            var productContext = new Mock<IProductStoreHiveContext>();
+            productContext.Setup(p => p.Hives).ReturnsEntitySet(new List<StoreHive>());
+
+            var userContext = new Mock<IUserContext>();
+            userContext.Setup(u => u.UserId).Returns(1);
+
+            var service = new HiveSectionService(productContext.Object, userContext.Object);
+
+            var createRequest = new UpdateHiveSectionRequest()
+            {
+                Name = "newHive",
+                Id = 1,
+                Code = "111341"
+            };
+            await service.CreateHiveSectionAsync(createRequest);
+            Assert.NotNull(service.GetHiveSectionsAsync());
+        }
+
+        [Fact]
+        public async void UpdateHiveSectionSuccessfull()
+        {
+            var productContext = new Mock<IProductStoreHiveContext>();
+            productContext.Setup(p => p.Hives).ReturnsEntitySet(new List<StoreHive>());
+
+            var userContext = new Mock<IUserContext>();
+            userContext.Setup(u => u.UserId).Returns(1);
+
+            var service = new HiveSectionService(productContext.Object, userContext.Object);
+
+            var updateRequest = new UpdateHiveSectionRequest()
+            {
+                Id = 1,
+                Code = "11",
+                Name = "name"
+            };
+
+            await service.UpdateHiveSectionAsync(1, updateRequest);
+        }
+
+        [Fact]
+        public async void SetStatusToHiveSectionSuccessfull()
+        {
+            var productContext = new Mock<IProductStoreHiveContext>();
+            productContext.Setup(p => p.Hives).ReturnsEntitySet(new List<StoreHive>());
+
+            var userContext = new Mock<IUserContext>();
+            userContext.Setup(u => u.UserId).Returns(1);
+
+            var service = new HiveSectionService(productContext.Object, userContext.Object);
+            await service.SetStatusAsync(1, true);
+
+            var hiveSection = await service.GetHiveSectionAsync(1);
+            Assert.True(hiveSection.IsDeleted);
+        }
+    }
+}
---
+++ KatlaSport.Services.Tests/HiveManagement/HiveServiceTests.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AutoFixture;
+using AutoFixture.Xunit2;
+using KatlaSport.DataAccess.ProductStoreHive;
+using KatlaSport.Services.HiveManagement;
+using Moq;
+using Xunit;
+
+namespace KatlaSport.Services.Tests.HiveManagement
+{
+    public class HiveServiceTests
+    {
+        [Fact]
+        public async void GetHivesAsyncReturnsNotNull()
+        {
+            var productContext = new Mock<IProductStoreHiveContext>();
+            productContext.Setup(p => p.Hives).ReturnsEntitySet(new List<StoreHive>());
+
+            var userContext = new Mock<IUserContext>();
+            userContext.Setup(u => u.UserId).Returns(1);
+
+            var service = new HiveService(productContext.Object, userContext.Object);
+
+            var hives = await service.GetHivesAsync();
+
+            Assert.NotNull(hives);
+        }
+
+        [Fact]
+        public async void GetHivesAsyncReturnsEmpty()
+        {
+            var productContext = new Mock<IProductStoreHiveContext>();
+            productContext.Setup(p => p.Hives).ReturnsEntitySet(new List<StoreHive>());
+
+            var userContext = new Mock<IUserContext>();
+            userContext.Setup(u => u.UserId).Returns(1);
+
+            var service = new HiveService(productContext.Object, userContext.Object);
+
+            var hives = await service.GetHivesAsync();
+
+            Assert.Empty(hives);
+        }
+
+        [Fact]
+        public async void CreateHiveSuccesFull()
+        {
+            var productContext = new Mock<IProductStoreHiveContext>();
+            productContext.Setup(p => p.Hives).ReturnsEntitySet(new List<StoreHive>());
+
+            var userContext = new Mock<IUserContext>();
+            userContext.Setup(u => u.UserId).Returns(1);
+
+            var service = new HiveService(productContext.Object, userContext.Object);
+
+            var createRequest = new UpdateHiveRequest
+            {
+                Name = "newHive",
+                Address = "address",
+                Code = "111341"
+            };
+            await service.CreateHiveAsync(createRequest);
+            Assert.NotNull(service.GetHivesAsync());
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public async void DeleteHiveSuccessfull(IFixture fixture, HiveService service,
+            [Frozen] Mock<IProductStoreHiveContext> productContext, [Frozen] Mock<IUserContext> userContext)
+        {
+            var hives = fixture.CreateMany<StoreHive>(5).ToList();
+            productContext.Setup(p => p.Hives).ReturnsEntitySet(hives);
+
+            await service.DeleteHiveAsync(1);
+        }
+
+        [Fact]
+        public async void UpdateHiveSuccessfull()
+        {
+            var productContext = new Mock<IProductStoreHiveContext>();
+            productContext.Setup(p => p.Hives).ReturnsEntitySet(new List<StoreHive>());
+
+            var userContext = new Mock<IUserContext>();
+            userContext.Setup(u => u.UserId).Returns(1);
+
+            var service = new HiveService(productContext.Object, userContext.Object);
+
+            var updateRequest = new UpdateHiveRequest
+            {
+                Address = "address",
+                Code = "11",
+                Name = "name"
+            };
+
+            await service.UpdateHiveAsync(1, updateRequest);
+        }
+
+        [Fact]
+        public async void SetStatusToHiveSuccessfull()
+        {
+            var productContext = new Mock<IProductStoreHiveContext>();
+            productContext.Setup(p => p.Hives).ReturnsEntitySet(new List<StoreHive>());
+
+            var userContext = new Mock<IUserContext>();
+            userContext.Setup(u => u.UserId).Returns(1);
+
+            var service = new HiveService(productContext.Object, userContext.Object);
+            await service.SetStatusAsync(1, true);
+
+            var hive = await service.GetHiveAsync(1);
+            Assert.True(hive.IsDeleted);
+        }
+    }
+}
